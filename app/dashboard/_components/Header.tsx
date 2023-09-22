eabdae13b405f66cdd0ad1cbbe70256f56308bc9
kamote-kid <kamote-kid@gmail.com>
2023-09-22 19:52:03 +0800

    make the navbar sticky

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -4,8 +4,8 @@ import UserMenu from "./UserMenu";
 
 function Header() {
   return (
-    <header className="p-4 border-b">
-      <div className="flex justify-between gap-4">
+    <header className="p-4 border-b sticky top-0 bg-white">
+      <div className="flex justify-between gap-4 px-4">
         <Image src="/logo.svg" width={40} height={40} alt="Logo" />
         <UserMenu />
       </div>
