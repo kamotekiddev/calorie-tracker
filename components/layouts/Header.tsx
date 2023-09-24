7213bc515aeae1b40067190e7f1cf7d1c5056aee
kamote-kid <kamote-kid@gmail.com>
2023-09-24 21:12:38 +0800

    remove the settings

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import { Settings } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import UserMenu from "./UserMenu";
+import Link from "next/link";
 
 function Header() {
   return (
@@ -9,10 +10,12 @@ function Header() {
       <div className="flex justify-between gap-4 mx-auto max-w-5xl p-4">
         <Image src="/logo.svg" width={40} height={40} alt="Logo" />
         <div className="flex items-center gap-4">
-          <Button className="flex gap-2" variant="outline">
-            Settings
-            <Settings className="h-4 w-4" />
-          </Button>
+          <Link href="/calorie-intakes/intake">
+            <Button className="flex gap-2">
+              Intake
+              <Plus className="h-4 w-4" />
+            </Button>
+          </Link>
           <UserMenu />
         </div>
       </div>
