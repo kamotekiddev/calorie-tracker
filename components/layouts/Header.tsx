97596a872fce2c6e318728b6935f84c74c845e21
kamote-kid <kamote-kid@gmail.com>
2023-09-24 21:17:45 +0800

    fix the add calorie intake history

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
             <div className='flex justify-between gap-4 mx-auto max-w-5xl p-4'>
                 <Image src='/logo.svg' width={40} height={40} alt='Logo' />
                 <div className='flex items-center gap-4'>
-                    <Link href='/calorie-intakes/intake'>
+                    <Link href='/dashboard/calorie-intakes/intake'>
                         <Button className='flex gap-2'>
                             Intake
                             <Plus className='h-4 w-4' />
