89029473d8c417d1d778f7a19ef43bf24d4dda5e
kamote-kid <kamote-kid@gmail.com>
2023-09-24 20:13:16 +0800

    decrease the padding

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -6,7 +6,7 @@ import UserMenu from "./UserMenu";
 function Header() {
   return (
     <header className="p-4 border-b sticky top-0 bg-white">
-      <div className="flex justify-between gap-4 px-4">
+      <div className="flex justify-between gap-4">
         <Image src="/logo.svg" width={40} height={40} alt="Logo" />
         <div className="flex items-center gap-4">
           <Button className="flex gap-2" variant="outline">
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,10 @@
 import CalorieConsumed from "./_components/CalorieConsumed";
 import CalorieIntakeHistory from "./_components/CalorieIntakeHistory";
-import CaloriePlans from "./_components/CaloriePlans";
 import TargetCalorie from "./_components/TargetCalorie";
-import WeightHistory from "./_components/WeightHistory";
 
 function Dashboard() {
   return (
-    <main className="p-8">
+    <main className="p-4">
       <div className="space-y-10">
         <div className="flex flex-col md:flex-row gap-2">
           <TargetCalorie />
