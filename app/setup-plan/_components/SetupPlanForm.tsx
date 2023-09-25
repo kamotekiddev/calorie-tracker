d79d43fea541008df58a19ec6f08ee53292671c2
kamote-kid <kamote-kid@gmail.com>
2023-09-25 11:57:33 +0800

    create a new setup plan

---
+++ app/setup-plan/_components/SetupPlanForm.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import FormInput from '@/components/form-elements/FormInput';
+import { Form } from '@/components/ui/form';
+import { PlanFields, PlanSchema } from '@/model/plan-sechema';
+import useCreatePlan from '@/hooks/usePlan';
+
+const defaultValues: PlanFields = {
+    target_calories: 1500,
+    name: '',
+};
+
+function SetupPlanForm() {
+    const router = useRouter();
+    const createPlan = useCreatePlan();
+
+    const form = useForm<PlanFields>({
+        defaultValues,
+        resolver: zodResolver(PlanSchema),
+    });
+
+    const onSubmit = async (values: PlanFields) => {
+        try {
+            await createPlan.mutateAsync(values);
+            router.replace('/dashboard');
+        } catch (error) {}
+    };
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='w-full max-w-xl'
+            >
+                <Card>
+                    <CardHeader>
+                        <CardTitle className='font-extrabold'>
+                            Create Plan
+                        </CardTitle>
+                        <CardDescription>
+                            Fill the details of your current plan
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className='space-y-4'>
+                        <FormInput
+                            label='Plan Name'
+                            name='name'
+                            control={form.control}
+                        />
+                        <FormInput
+                            label='Target Calorie'
+                            name='target_calories'
+                            control={form.control}
+                        />
+                    </CardContent>
+                    <CardFooter className='flex justify-end'>
+                        <Button disabled={createPlan.isLoading} type='submit'>
+                            Create Plan
+                        </Button>
+                    </CardFooter>
+                </Card>
+            </form>
+        </Form>
+    );
+}
+
+export default SetupPlanForm;
--- app/setup-plan/page.tsx
+++ app/setup-plan/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import SetupPlanForm from './_components/SetupPlanForm';
 
 function SetupPlan() {
-    return <div>SetupPlan</div>;
+    return (
+        <main className='p-4 grid place-items-center h-screen'>
+            <SetupPlanForm />
+        </main>
+    );
 }
 
 export default SetupPlan;
